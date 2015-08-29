26f615e80e948ae3422fc7b976e5954d9a02f57b
Phillip Creglow <phillipcreglow@msn.com>
2015-08-28 21:07:43 -0400

    Followed course example for prepending name and role in header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,25 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Phillip Creglow");
+ // $("#main").append("Phillip Creglow");
+
+ var awesomeThoughts = "I am Phillip and I am AWESOME!";
+ console.log(awesomeThoughts);
+
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+ $("#main").append(funThoughts);
+
+ var email = "phillipcreglow@msn.com";
+
+ var newEmail = email.replace("msn", "gmail");
+ console.log(email);
+ console.log(newEmail);
+
+ var formattedName = HTMLheaderName.replace("%data%", "Phillip Creglow");
+
+ var role = "Junior Web Developer";
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
