9b14b574ad532eb79a871a39d23c4f201983941f
Phillip Creglow <phillipcreglow@msn.com>
2015-09-12 19:54:58 -0400

    Updated resumeBuilder.js with work, education, bio JSON data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,18 +3,21 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 // $("#main").append("Phillip Creglow");
 
+/*
 var awesomeThoughts = "I am Phillip and I am AWESOME!";
 console.log(awesomeThoughts);
 
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
 $("#main").append(funThoughts);
+*/
 
-var email = "phillipcreglow@msn.com";
+/*var email = "phillipcreglow@msn.com";
 
 var newEmail = email.replace("msn", "gmail");
 console.log(email);
 console.log(newEmail);
+*/
 
 var formattedName = HTMLheaderName.replace("%data%", "Phillip Creglow");
 
@@ -37,37 +40,53 @@ var bio = {
   "photo": "images/fry.jpg"
 };
 
-$("#main").append(bio.role);
+//$("#main").append(bio.role);
 
 var work = {
-  "position": "Junior Web Developer/Strategic Political Consultant",
-  "employer": "TargetSmart Communications",
-  "years": "September 2015",
-  "city": "Orlando, FL"
+  "jobs": [{
+    "position": "Junior Web Developer/Strategic Political Consultant",
+    "employer": "TargetSmart Communications",
+    "years": "September 2015 - Present",
+    "city": "Orlando, FL"
+  }, {
+    "position": "Regional Political Director",
+    "employer": "American Medical Association",
+    "years": "April 2011 - January 2014",
+    "city": "Washington, DC"
+  }, {
+    "position": "Campaign Services/Political Representative",
+    "employer": "National Association of REALTORS",
+    "years": "April 2007 - April 2011",
+    "city": "Washington, DC"
+  }, {
+    "position": "Government Affairs Director",
+    "employer": "Kentucky Associaiton of REALTORS",
+    "years": "January 2006 - April 2007",
+    "city": "Lexington, KY"
+  }, {
+    "position": "Public Policy Communications/Field Director",
+    "employer": "Michigan Association of REALTORS",
+    "years": "February 2004 - January 2006",
+    "city": "Lansing, MI"
+  }]
 };
 
-$("#main").append(work["position"]);
+//$("#main").append(work["position"]);
 
 var education = {
-  "schools": [
-    {
-      "name": "Western Michigan University",
-      "city": "Kalamazoo, MI",
-      "degree": "BA",
-      "major": "Public Administration",
-      "graduation": "December 2003"
-    },
-    {
-      "name": "The Iron Yard",
-      "city": "Orlando, FL",
-      "degree": "Certificate of Completion",
-      "major": "Front End Engineering",
-      "graduation": "August 2015"
-    }
-  ]
+  "schools": [{
+    "name": "Western Michigan University",
+    "city": "Kalamazoo, MI",
+    "degree": "BA",
+    "major": "Public Administration",
+    "graduation": "December 2003"
+  }, {
+    "name": "The Iron Yard",
+    "city": "Orlando, FL",
+    "degree": "Certificate of Completion",
+    "major": "Front End Engineering",
+    "graduation": "August 2015"
+  }]
 };
-  education["school"] = "Western Michigan University",
-  education["years"] = "1999 - 2003",
-  education["location"] = "Kalamazoo, MI"
 
-$("#main").append(education.school);
+//$("#main").append(education.school);
